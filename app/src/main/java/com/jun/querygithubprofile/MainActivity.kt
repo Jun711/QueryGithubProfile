a8219f6cf873fdabc5674a40f1791453a5f2c75b
--get-all <cygoh@umich.edu>
2017-06-08 23:57:08 -0700

    Retrofit Builder

--- app/src/main/java/com/jun/querygithubprofile/MainActivity.kt
+++ app/src/main/java/com/jun/querygithubprofile/MainActivity.kt
@@ -2,11 +2,37 @@ package com.jun.querygithubprofile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.GsonBuilder
+import io.realm.RealmObject
+import retrofit.GsonConverterFactory
+import retrofit.Retrofit
+import retrofit.RxJavaCallAdapterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gson = GsonBuilder().setExclusionStrategies(object : ExclusionStrategy {
+            override fun shouldSkipField(f: FieldAttributes): Boolean {
+                return f.declaringClass == RealmObject::class.java
+            }
+
+            override fun shouldSkipClass(clazz: Class<*>): Boolean {
+                return false
+            }
+        }).create()
+
+        val retrofit: Retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl("https://api.github.com/")
+                .build()
+
+        val githubService: GithubService = retrofit.create(
+                GithubService::class.java)
     }
 }
